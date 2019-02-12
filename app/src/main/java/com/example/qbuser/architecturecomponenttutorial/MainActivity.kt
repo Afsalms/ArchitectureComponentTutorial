4d1780470239edb95e77b4ae91392f57c5ac972d
afsals <afsals@qburst.com>
2019-02-12 09:22:10 +0530

    Added button and click listener

--- app/src/main/java/com/example/qbuser/architecturecomponenttutorial/MainActivity.kt
+++ app/src/main/java/com/example/qbuser/architecturecomponenttutorial/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.qbuser.architecturecomponenttutorial
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            if(editText.text.toString().length > 0){
+                textView.text = textView.text.toString()+ "\n" + editText.text.toString()
+                editText.text = null
+            }
+        }
     }
 }
